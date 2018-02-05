fdc60766088a506b7b10e6643e4e99f3dc1d2332
dgangatharan <dinesh.gangatharan@authada.de>
2018-02-05 17:45:18 +0100

    Multiproject structure example

---
+++ wrapper/src/main/java/de/authada/wrapper/WrapperActivity.java
@@ -0,0 +1,13 @@
+package de.authada.wrapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WrapperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wrapper);
+    }
+}
